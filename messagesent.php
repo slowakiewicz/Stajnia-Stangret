b5c40ea5f5a917eabcf44482b649caad6a786752
slowakiewicz <36136838+slowakiewicz@users.noreply.github.com>
2018-07-04 18:58:17 +0200

    init commit

---
+++ messagesent.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Stajnia Stangret</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+</head>
+
+<body>
+    <div id="fullsite">
+        <header id="messagesent" class="centeredtext ">
+            <h1 class="opensans extrabold upper whitetext blockelement">Wiadaomość została wysłana.
+                <span class="opensans upper lighttext thickspan whitetext blockelement">Dziękujemy za kontakt.</span>
+            </h1>
+        </header>
+    </div>
+</body>
+
+</html>
